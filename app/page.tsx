26e3bae6550dccb60857a5edaf95a33bc97dc51b
muhafs <muhamadhaspin@gmail.com>
2024-11-18 09:57:42 +0700

    complete hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from '@/components/Hero'
+
 export default function Home() {
-	return <main className=""></main>
+	return (
+		<main className="flex flex-col items-center px-4">
+			<Hero />
+		</main>
+	)
 }
