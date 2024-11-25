2ab529e0e288036fb58abd6e617800ebab86c592
muhafs <muhamadhaspin@gmail.com>
2024-11-25 10:48:02 +0700

    imporve about paragraph

--- components/About.tsx
+++ components/About.tsx
@@ -18,15 +18,23 @@ export default function About() {
 			<SectionHeading>About me</SectionHeading>
 
 			<p className="mb-3">
-				After graduating with a degree in <span className="font-medium">Sharia specialty</span>, I decided to pursue my passion for programming. I enrolled in a coding bootcamp and learned{' '}
+				{/* After graduating with a degree in <span className="font-medium">Sharia specialty</span>, I decided to pursue my passion for programming. I enrolled in a coding bootcamp and learned{' '}
 				<span className="font-medium">full-stack web development</span>. <span className="italic">My favorite part of programming</span> is the problem-solving aspect. I <span className="underline">love</span> the feeling of finally figuring
 				out a solution to a problem. My core stack is <span className="font-medium">{joinWithAnd(skill.core)}</span>. I am also familiar with {joinWithAnd(skill.other)}. I am always looking to learn new technologies. I am currently looking
 				for a <span className="font-medium">full-time position</span> as a software developer.
+				<br /> */}
+				After earning a degree in <span className="font-medium">Sharia specialty</span>, I discovered my passion for programming and pursued it through a coding bootcamp, where I mastered{' '}
+				<span className="font-medium">full-stack web development</span>. Problem-solving is <span className="italic">My favorite part of programming</span> —I thrive on finding creative solutions. My core stack includes{' '}
+				<span className="font-medium">{joinWithAnd(skill.core)}</span>, and I'm also experienced with {joinWithAnd(skill.other)}. Always eager to learn, I'm now seeking a <span className="font-medium">full-time position</span> as a software
+				developer to grow and contribute my skills.
 			</p>
 
 			<p>
-				<span className="italic">When I'm not coding</span>, I enjoy {joinWithAnd(skill.hobby)}. I also enjoy <span className="font-medium">learning new things</span>. I am currently learning about{' '}
+				{/* <span className="italic">When I'm not coding</span>, I enjoy {joinWithAnd(skill.hobby)}. I also enjoy <span className="font-medium">learning new things</span>. I am currently learning about{' '}
 				<span className="font-medium">{joinWithAnd(skill.interest)}</span>. I'm also learning how to {joinWithAnd(skill.current)}.
+				<br /> */}
+				<span className="italic">When I'm not coding</span>, I enjoy {joinWithAnd(skill.hobby)}. I have a passion for learning and am currently exploring <span className="font-medium">{joinWithAnd(skill.interest)}</span>, and teaching myself
+				to {joinWithAnd(skill.current)}.
 			</p>
 		</motion.section>
 	)
