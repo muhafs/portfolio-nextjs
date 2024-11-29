885960f1e74d58a2d3f17f783304597d6fd51e66
muhafs <muhamadhaspin@gmail.com>
2024-11-29 16:21:49 +0700

    move skilss data into data file

--- components/About.tsx
+++ components/About.tsx
@@ -4,20 +4,9 @@ import joinWithAnd from "@/helpers/joinWithAnd";
 import SectionHeading from "./SectionHeading";
 import { motion } from "framer-motion";
 import { useActiveView } from "@/lib/hooks";
+import { aboutData } from "@/lib/data";
 
 export default function About() {
-    const skill = {
-        core: ["React", "Next.js", "Node", "MonggoDB"],
-        other: ["Typescript", "Prisma"],
-        hobby: [
-            "playing video games",
-            "watching movies",
-            "spent some time with my family",
-        ],
-        interest: ["history", "philosophy"],
-        current: ["play with guitar"],
-    };
-
     const { ref } = useActiveView({ sectionName: "About", threshold: 0.75 });
 
     return (
@@ -45,9 +34,11 @@ export default function About() {
                 Problem-solving is{" "}
                 <span className="italic">My favorite part of programming</span>{" "}
                 —I thrive on finding creative solutions. My core stack includes{" "}
-                <span className="font-medium">{joinWithAnd(skill.core)}</span>,
-                and I'm also experienced with {joinWithAnd(skill.other)}. Always
-                eager to learn, I'm now seeking a{" "}
+                <span className="font-medium">
+                    {joinWithAnd(aboutData.core)}
+                </span>
+                , and I'm also experienced with {joinWithAnd(aboutData.other)}.
+                Always eager to learn, I'm now seeking a{" "}
                 <span className="font-medium">full-time position</span> as a
                 software developer to grow and contribute my skills.
             </p>
@@ -57,12 +48,12 @@ export default function About() {
 				<span className="font-medium">{joinWithAnd(skill.interest)}</span>. I'm also learning how to {joinWithAnd(skill.current)}.
 				<br /> */}
                 <span className="italic">When I'm not coding</span>, I enjoy{" "}
-                {joinWithAnd(skill.hobby)}. I have a passion for learning and am
-                currently exploring{" "}
+                {joinWithAnd(aboutData.hobby)}. I have a passion for learning
+                and am currently exploring{" "}
                 <span className="font-medium">
-                    {joinWithAnd(skill.interest)}
+                    {joinWithAnd(aboutData.interest)}
                 </span>
-                , and teaching myself to {joinWithAnd(skill.current)}.
+                , and teaching myself to {joinWithAnd(aboutData.current)}.
             </p>
         </motion.section>
     );
--- helpers/joinWithAnd.ts
+++ helpers/joinWithAnd.ts
@@ -1,9 +1,9 @@
 export default function joinWithAnd(arr: string[]): string {
-	if (arr.length === 0) return '' // If the array is empty, return an empty string
-	if (arr.length === 1) return arr[0] // If there's only one item, return it directly
-	if (arr.length === 2) return arr.join(' and ') // If there are two items, join them with 'and'
+    if (arr.length === 0) return ""; // If the array is empty, return an empty string
+    if (arr.length === 1) return arr[0]; // If there's only one item, return it directly
+    if (arr.length === 2) return arr.join(" and "); // If there are two items, join them with 'and'
 
-	// For arrays with more than two items, join with commas and add 'and' before the last item
-	const lastItem = arr.pop() // Remove the last item from the array
-	return arr.join(', ') + ' and ' + lastItem // Join the remaining items with commas, then add 'and' before the last item
+    // For arrays with more than two items, join with commas and add 'and' before the last item
+    const lastItem = arr.pop(); // Remove the last item from the array
+    return arr.join(", ") + " and " + lastItem; // Join the remaining items with commas, then add 'and' before the last item
 }
